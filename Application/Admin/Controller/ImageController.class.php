95c353904c7167b17eb34485f138564a5c5dc350
MasterHo1228 <masterho1228@gmail.com>
2016-12-10 20:35:31 +0800

    ImageController -- move upload and crop image code to model class

--- Application/Admin/Controller/ImageController.class.php
+++ Application/Admin/Controller/ImageController.class.php
@@ -25,34 +25,23 @@ class ImageController extends Controller
         //清除上次上传的图片的url
         session('uploadHeaderImgUrl', null);
         if (checkSalesUserLogin()) {
-            $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize = 3145728;// 设置附件上传大小
-            $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->rootPath = '.' . UPLOAD_PATH . 'header/'; // 设置附件上传根目录
-            $upload->autoSub = false; // 设置自动使用子目录保存上传文件
-            // 上传文件
-            $info = $upload->upload();
-            if (!$info) {// 上传错误
-                //AJAX返回错误信息（前端插件尚未正常显示）
-                $data['error'] = $upload->getError();
-            } else {// 上传成功
-                //获取裁剪图片数据的json数组
-                $avatarData = $_POST['avatar_data'];
-                $avatarArr = json_decode($avatarData,true);
-
-                //图片上传到服务器后的相对路径
-                $imgPath = './Public/uploads/header/'.$info['avatar_file']['savename'];
-                $image = new \Think\Image();
-                //打开图片
-                $image->open($imgPath);
-                //剪裁图片并覆盖保存
-                $image->crop($avatarArr['width'], $avatarArr['height'],$avatarArr['x'],$avatarArr['y'])->save($imgPath);
-
-                //用session暂存图片的路径，稍后添加商品的时候一并写入数据库
-                $url = DEFAULT_HEADER_PIC_PATH.$info['avatar_file']['savename'];
-                //AJAX返回上传处理后的图片url
-                $data['result'] = $url;
-                session('uploadHeaderImgUrl', $url);
+            $model = new \Admin\Model\ImageModel();
+            $info = $model->upload('header/');
+            if ($info) {
+                $avatarArr = json_decode($_POST['avatar_data'], true);
+                $filePath = './Public/uploads/header/';
+                $fileName = $info['avatar_file']['savename'];
+                if ($model->cropImage($avatarArr, $filePath, $fileName)) {
+                    //用session暂存图片的路径，稍后添加商品的时候一并写入数据库
+                    $url = DEFAULT_HEADER_PIC_PATH . $info['avatar_file']['savename'];
+                    //AJAX返回上传处理后的图片url
+                    $data['result'] = $url;
+                    session('uploadHeaderImgUrl', $url);
+                } else {
+                    $data['error'] = 'Crop image failed!';
+                }
+            } else {
+                $data['error'] = $info;
             }
             $this->ajaxReturn($data);
         }
@@ -64,22 +53,16 @@ class ImageController extends Controller
     public function uploadImg()
     {
         if (checkSalesUserLogin()) {
-            $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize = 3145728;// 设置附件上传大小
-            $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->rootPath = '.' . UPLOAD_PATH; // 设置附件上传根目录
-
-            //上传图片的路径
-            $urlPrefix = URL . UPLOAD_PATH;
-            // 上传单个文件
-            $info = $upload->upload();
-            if (!$info) {// 上传错误提示错误信息
-                $this->ajaxReturn('error|' . $upload->getError(), 'EVAL');
-            } else {// 上传成功 获取上传文件信息
+            $model = new \Admin\Model\ImageModel();
+            $info = $model->upload('', true);
+            if ($info) {
+                $urlPrefix = URL . UPLOAD_PATH;
                 foreach ($info as $file) {
                     $url = $urlPrefix . $file['savepath'] . $file['savename'];
                     $this->ajaxReturn($url, 'EVAL');
                 }
+            } else {
+                $this->ajaxReturn('error|' . $info, 'EVAL');
             }
         }
     }
---
+++ Application/Admin/Model/ImageModel.class.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MasterHo
+ * Date: 2016/12/10
+ * Time: 19:46
+ */
+
+namespace Admin\Model;
+
+/**
+ * 图片处理独立模型类
+ * Class ImageModel
+ * @package Admin\Model
+ * @author MasterHo1228
+ */
+class ImageModel
+{
+    /**
+     * 上传图片
+     * @param string $folder 上传图片子路径，默认为空
+     * @param bool $autoSub 是否自动创建以日期命名的子目录，默认为false
+     * @return array|bool|string
+     */
+    public function upload($folder = '', $autoSub = false)
+    {
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize = 3145728;// 设置附件上传大小
+        $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath = '.' . UPLOAD_PATH . $folder; // 设置附件上传根目录
+        $upload->autoSub = $autoSub; // 设置自动使用子目录保存上传文件
+        // 上传文件
+        $info = $upload->upload();
+
+        if (!$info) {// 上传错误
+            //返回错误信息
+            return $upload->getError();
+        } else {
+            //返回信息数组
+            return $info;
+        }
+    }
+
+    /**
+     * 裁剪图片
+     * @param array $cropData 裁剪图片参数数据
+     * @param string $filePath 文件目录
+     * @param string $fileName 文件名
+     * @return bool
+     */
+    public function cropImage($cropData, $filePath, $fileName)
+    {
+        if (!empty($cropData) && !empty($filePath) && !empty($fileName)) {
+            $image = new \Think\Image();
+            //打开图片
+            $imgUrl = $filePath . $fileName;
+            $image->open($imgUrl);
+            //剪裁图片并覆盖保存
+            return $image->crop($cropData['width'], $cropData['height'], $cropData['x'], $cropData['y'])->save($imgUrl);
+        } else {
+            return false;
+        }
+    }
+
+}
