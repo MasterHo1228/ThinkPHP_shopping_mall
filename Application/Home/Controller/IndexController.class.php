338784a0b1819b97cfbe6c2f343807628499dbda
MasterHo1228 <masterho1228@gmail.com>
2016-12-12 12:15:52 +0800

    Home module -- Initialize data display in first page.

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,6 +7,24 @@ class IndexController extends Controller
 {
     public function index()
     {
+        $model = new \Home\Model\GoodsModel();
+        $list = $model->getIndexList();
+        $top3 = $model->getTop3Goods();
+
+        for ($i = 0; $i < count($top3); $i++) {
+            $top3[$i]['gdescription'] = html_entity_decode($top3[$i]['gdescription'], ENT_NOQUOTES, "utf-8");
+            $top3[$i]['gdescription'] = strip_tags($top3[$i]['gdescription']);
+        }
+
+        $rowCount = count($list) / 3;
+        if ($rowCount < 1) {
+            $rowCount = 1;
+        }
+
+        dump($list);
+        $this->assign('list', $list);
+        $this->assign('rowCount', $rowCount);
+        $this->assign('top3', $top3);
         layout('Layout/layout');
         $this->display();
     }
--- Application/Home/Model/GoodsModel.class.php
+++ Application/Home/Model/GoodsModel.class.php
@@ -17,6 +17,13 @@ class GoodsModel
         return $table->where($where)->order('gStatus DESC')->field('gID,gName,gPrice,gPhoto,gStatus')->limit(30)->select();
     }
 
+    public function getTop3Goods()
+    {
+        $table = M('viewgoodsdetail');
+        $data = $table->where("gStatus='2'")->field('gID,gName,gPrice,gPhoto,gDescription')->limit(3)->select();
+        return $data;
+    }
+
     public function getDetailByID($id)
     {
         if (!empty($id)) {
